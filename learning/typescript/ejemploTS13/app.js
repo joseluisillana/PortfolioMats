3b07f893bfa019bf0fe5e60e85d8bda078492c38
joseluisillana <joseluisillana@gmail.com>
2017-10-22 21:33:19 +0200

    Decoradores de clases

---
+++ learning/typescript/ejemploTS13/app.js
@@ -0,0 +1,29 @@
+/**
+ Decoradores de clases
+
+ Característica experimental, permite abstraer funcionalidades
+
+ Para que el typescript no suelte un error ya que son experimentales,
+ Se añade al tsconfig la linea:
+ "experimentalDecorators":true,
+**/
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+//Un decorator es una simple función que acepta una función
+function consola(constructor) {
+    console.log(constructor);
+}
+// Se setean con la arroba y el nombre d ela función a usar
+var Villano = /** @class */ (function () {
+    function Villano(nombre) {
+        this.nombre = nombre;
+    }
+    Villano = __decorate([
+        consola
+    ], Villano);
+    return Villano;
+}());
