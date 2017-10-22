8c40ee83d52d6ca299a3ed5998ff06193f33a905
joseluisillana <joseluisillana@gmail.com>
2017-10-22 21:14:34 +0200

    Promesas

--- learning/typescript/ejemploTS9/app.js
+++ learning/typescript/ejemploTS9/app.js
@@ -11,7 +11,12 @@ var promesa1 = new Promise(function (resolve, reject) {
         // Termina bien
         resolve();
         // Termina mal
-        reject();
+        //reject();
     }, 1500);
 });
+// ¿Comómo se ejecuta?
+console.log("Paso 1 << Tarea sincrona 1");
 promesa1.then(function () { console.log("Ejecutarme cuando termine bien"); }, function () { console.error("Ejecutar cuando algo sale mal"); });
+console.log("Paso 2 << Tarea sincrona 2");
+// Nota que como una promesa es una tarea 'asincrona', los console.log, 
+// terminarán antes y aparecerán antes en la consola dle navegador. 
